d045458be67ab2ea136b2a7acebf36607366ef09
Ruven <ruven-bellmann@web.de>
2024-10-16 14:52:25 +0300

    :memo: Inserted old config values

--- common/src/main/java/net/pandarix/beautify/Config.java
+++ common/src/main/java/net/pandarix/beautify/Config.java
@@ -4,7 +4,9 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class Config
 {
-    public static ModConfigSpec.DoubleValue TEST;
+    public static ModConfigSpec.IntValue SEARCHRADIUS;
+    public static ModConfigSpec.BooleanValue OPENS_FROM_ROOT;
+    public static ModConfigSpec.IntValue BOTANIST_SPAWN_WEIGHT;
     public static ModConfigSpec SPEC;
 
     static
@@ -16,11 +18,22 @@ public class Config
 
     private static void setupConfig(ModConfigSpec.Builder builder)
     {
-        builder.push("test");
+        builder.push("Blinds");
 
-        TEST = builder
-                .comment("Testcomment. Default: 0.75")
-                .defineInRange("Test", 0.75, 0.0, 1.0);
+        SEARCHRADIUS = builder.comment(
+                        "Searches X-Blocks below and X/2 to the sides of the clicked blind for others and opens/closes them too")
+                .defineInRange("searchRadius", 6, 0, 100);
+
+        OPENS_FROM_ROOT = builder.comment(
+                "Opens blinds from the topmost blind on if true").define("opensFromRoot", true);
+
+        builder.pop();
+
+        builder.push("Generation");
+
+        BOTANIST_SPAWN_WEIGHT = builder.comment(
+                        "Defines the chance of a Botanist Villager House inside a village")
+                .defineInRange("botanistSpawnWeight", 2, 0, Integer.MAX_VALUE);
 
         builder.pop();
     }
